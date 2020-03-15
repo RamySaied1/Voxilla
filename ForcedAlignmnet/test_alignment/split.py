d8dc3d902bf0ead098b2434e572eaa46684e07a2
shehabalaa <shehabalaa97@gmail.com>
2020-03-15 12:39:34 +0200

    fix small bug in split.py

--- ForcedAlignmnet/test_alignment/split.py
+++ ForcedAlignmnet/test_alignment/split.py
@@ -10,20 +10,26 @@ with open("alignment.txt") as f:
 ranges = list(map(int,ranges))
 ranges = list(zip(ranges[::2], ranges[1::2]))
 
-print(ranges)
 data, samplerate = sf.read("test.flac")
-samplerate = 16000
+assert(samplerate == samplerate)
 frame_duration=0.025; frame_shift=0.010
 
-win_size = int(np.floor(frame_duration * samplerate))
-win_shift = int(np.floor(frame_shift * samplerate))
+win_size = int(frame_duration * samplerate)
+win_shift = int(frame_shift * samplerate)
 
+print(f"frame count is: {(len(data)-win_size+win_shift)/win_shift}")
 
 def samples(n):
-    return win_size + win_shift*(n)
+    intial_shift = win_size-win_shift if n >0 else 0
+    return intial_shift + win_shift*n
 
 for i,((start,end), word) in enumerate(zip(ranges,words)):
     start = samples(start)
     end = samples(end)
     print(start, end)
+    if(start>=len(data)):
+        print(f"range exeded frames limit which is {len(data)}")
+        exit()
     sf.write(f'./segments/{i}_{word}.flac', data[start:end], samplerate)
+
+print(f"OK Last number should be {len(data)}")
