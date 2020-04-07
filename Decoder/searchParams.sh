2e7869b450c4333ae67f3cdbb39eb931700712a3
shehabalaa <shehabalaa97@gmail.com>
2020-04-07 22:06:26 +0200

    Fix searchParams.sh script

--- Decoder/searchParams.sh
+++ Decoder/searchParams.sh
@@ -11,7 +11,7 @@ fi
 
 graphFolder=./Graphs/$1/
 activationsFolder=./Activations/$2/
-filesFile=$activationsFolder$3
+filesFile=$3
 maxActiveTokens=$4
 beam=$5
 amwStart=$6
@@ -24,36 +24,41 @@ echo graphFolder: $graphFolder, activationsFolder: $activationsFolder, filesFile
 echo maxActiveTokens is $maxActiveTokens , beam is $beam, amwStart $amwStart, amwEnd $amwEnd, amwStep $amwStep
 echo ReferencePath fie is $refFilePath
 
-if [ ! $amwStart ]
-then
-    amwStart=`cat reports.txt | grep -o "[0-9]\+.[0-9]*"`
-    if [ ! $amwStart ]
-    then
-        amwStart="1."
-    fi
-fi
 
 params=$graphFolder\ $activationsFolder\ $filesFile\ $maxActiveTokens\ $beam\ $amwStart\ $amwEnd\ $amwStep
 params_=$1\_$2\_$3\_$4\_$5\_$6\_$7\_$8\_on_$refFileLen\_examples
 outFilePath=./PredTrans/out_$params_.txt
 
-g++ -g ./C++/*.cpp
-./a.out  $params > $outFilePath 
+lasAmwStart=`cat ./PredTrans/out_$params_.txt | grep "amw:" | tail -n 1 | grep -o "[0-9]\+.\?[0-9]*"`
+if [ $lasAmwStart ]
+then
+    let amwStart="lasAmwStart+$amwStep"
+    echo "Continue on perivous amw amwStart set to $amwStart"
+fi
 
-sed -i "/Token/d;/^\s*$/d;s/ $//g;/in:/d" $outFilePath
+
+# g++ -g ./C++/*.cpp
+# ./a.out  $params >> $outFilePath 
+
+sed -i "/Token/d;/^\s*$/d;/in:/d" $outFilePath
 trash-put ./PredTrans/Split/*
 
 
-let refFileLen="refFileLen+1"
 echo > reports.txt
+let refFileLen="refFileLen+2"
 split -l $refFileLen $outFilePath -d ./PredTrans/Split/
 for file in `ls ./PredTrans/Split/`;
 do
-    echo "-------------------------------------------------------------" >> reports.txt
-    tail -n 1 ./PredTrans/Split/$file >>  reports.txt
-    sed -i "/amw:/d" ./PredTrans/Split/$file
-    sed -i "s/\!//g" ./PredTrans/Split/$file
-    python3 ../PerformanceMeasure/performance.py -hyp ./PredTrans/Split/$file  -ref $refFilePath | tail -n 2 >> reports.txt
+    numOfLines=`cat ./PredTrans/Split/$file | wc -l`
+    if [ "$numOfLines" == "$refFileLen" ] 
+    then
+        echo "-------------------------------------------------------------" >> reports.txt
+        tail -n 1 ./PredTrans/Split/$file >>  reports.txt
+        sed -i "/amw:/d" ./PredTrans/Split/$file
+        python3 ../PerformanceMeasure/performance.py -hyp ./PredTrans/Split/$file  -ref $refFilePath | tail -n 2 >> reports.txt
+    else
+        echo "Wrong number of lines discard split: $file"
+    fi
     echo "-------------------------------------------------------------" >> reports.txt
 done
 cat reports.txt
