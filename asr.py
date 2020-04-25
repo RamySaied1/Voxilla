034415728b8bf094715ba38f558ee6ac08e4cb13
shehabalaa <shehabalaa97@gmail.com>
2020-04-25 22:31:26 +0200

    Done integration with forced alignment

--- asr.py
+++ asr.py
@@ -1,35 +1,41 @@
 from os.path import join as path_join
 import sys
-sys.path.append("./Decoder/C++/")
+sys.path.append("./Decoder/C++/PythonBinding/")
 sys.path.append("./Classifier/")
 sys.path.append("./FeatureExtraction/")
+sys.path.append("./ForcedAlignmnet/")
 from decoder_wrapper import PyDecoder as Decoder
 from classifier import Classifier
+from Transcript import Transcript
 from itertools import groupby
 from Wav2Feat import wav_to_feat
 import numpy as np
 
 class ASR:
-    def __init__(self, model_file, fst_folder, fst_input_labels_file):
+    def __init__(self, model_file, fst_folder, acoustic_model_labels_file, words_lexicon_file = "./ForcedAlignmnet/words_lexicon.txt", phones_lexicon_file = "./ForcedAlignmnet/phones_lexicon.txt"):
         if fst_folder and fst_folder[-1] != '/': fst_folder+="/"
-        self.decoder = Decoder(fst_folder, path_join(fst_folder,fst_input_labels_file)) 
+        self.decoder = Decoder(fst_folder, acoustic_model_labels_file) 
         self.classifier = Classifier(model_file)
+        self.trans = Transcript(words_lexicon_file, phones_lexicon_file)
 
-    def speech_to_text(self, wav_file, max_active_tokens, beam_width, acoustic_model_weigh):
+    def speech_to_text(self, wav_file, max_active_tokens, beam_width, acoustic_model_weigh, include_alignment = False):
         features = np.transpose(np.array(self._get_features(wav_file)))
         states_seq = self._decode(features, max_active_tokens,beam_width, acoustic_model_weigh)
-        return ' '.join(self._get_words_from_states_seq(states_seq))
-
-    def speech_to_words_aligned(self, wav_file, max_active_tokens, beam_width, acoustic_model_weigh):
-        pass
+        if(not include_alignment):
+            states_seq = self._remove_selfloops(states_seq)
+            return ' '.join(self._get_words_from_states_seq(states_seq))
+        else:
+            frames_count = len(features)
+            return self.trans.get_transcript(states_seq,frames_count)
 
     def _decode(self, features, max_active_tokens, beam_width, acoustic_model_weight):
         activations = self.classifier.eval(features, do_stack_features=False)
         return self.decoder.decode(activations, max_active_tokens, beam_width, 1/acoustic_model_weight)
     
+    def _remove_selfloops(self,states_seq):
+        return [state for i,state in enumerate(states_seq) if not i or states_seq[i][-1] != states_seq[i-1][-1]] # remove self loops
+
     def _get_words_from_states_seq(self, states_seq):
-        # remove self loops
-        states_seq = [state for i,state in enumerate(states_seq) if not i or states_seq[i][-1] != states_seq[i-1][-1]]
         return [outlabel for _,outlabel,_ in states_seq if not self.decoder.is_special_sym(outlabel)]
 
     def _get_features(self, wav_file):
--- test_asr.py
+++ test_asr.py
@@ -6,12 +6,13 @@ import sys;
 from time import time
 from asr import ASR
 
-# python3 test_asr.py -model ./Experiments/BLSTM1/BLSTM_CE -fst_folder ./Decoder/Graphs/200k-vocab/ -input_labels_file labels.ciphones -srcfile waves_test.txt -outfile transcript
+# python3 test_asr.py -model ./Experiments/BLSTM1/BLSTM_CE -fst_folder ./Decoder/Graphs/200k-vocab/ -acoustic_model_labels_file ./Decoder/Graphs/200k-vocab/labels.ciphones -srcfile waves_test.txt -outfile transcript
+
 def main():
     parser = argparse.ArgumentParser( description="Decode speech from parameter files.")
     parser.add_argument('-model', '--model', help='classifier file', required=True, default=None)
     parser.add_argument('-fst_folder', '--fst_folder', help="folder containg HCLG graph", required=True, default=None)
-    parser.add_argument('-input_labels_file', '--input_labels_file', help="Text file name containing input labels", required=True, default=None)
+    parser.add_argument('-acoustic_model_labels_file', '--acoustic_model_labels_file', help="Text file name containing input labels", required=True, default=None)
     parser.add_argument('-srcfile', '--srcfile', help='file containing wave files pathes to test on', required=True, default=None)
     parser.add_argument('-outfile', '--outfile', help='Filename to write output hypotheses', required=True, default=None)
     parser.add_argument('-amw', '--amw', help='Relative weight of LM score', required=False, type=float, default=9)
@@ -20,7 +21,7 @@ def main():
 
     args = parser.parse_args()
 
-    asr = ASR(args.model, args.fst_folder, args.input_labels_file)
+    asr = ASR(args.model, args.fst_folder, args.acoustic_model_labels_file)
 
     all_time_start = time()
     predictedSents = []
@@ -29,7 +30,7 @@ def main():
             with open(args.outfile, 'w', buffering=1) as fout:
                 all_time_start = time()
                 for line in ftest.read().split():
-                    text = asr.speech_to_text(line,1000,12,3)
+                    text = asr.speech_to_text(line,1000,12,3,include_alignment=False)
                     predictedSents.append(text)
     except KeyboardInterrupt:
         print("[CTRL+C detected]")
