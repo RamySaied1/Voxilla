9a84092e7cf13ca80f460fe507a1f237aa9b84e1
Ramy Saied <ramy.m.saied@gmail.com>
2020-05-08 03:57:28 +0200

    add interfernce to keras model

--- Classifier/classifier.py
+++ Classifier/classifier.py
@@ -4,8 +4,10 @@ import os.path
 import re
 import sys
 import cntk
+from keras.models import load_model
 
-class Classifier:
+
+class Classifier_cntk:
     def __init__(self,model_filename: str):
         self.model = None
         try:
@@ -53,3 +55,28 @@ class Classifier:
             return self.model.eval(feature_vectors.astype('f'))[0]
         else:
             raise  Exception("Model isn't loaded yet")
+
+
+class Classifier_keras:
+  def __init__(self,model_filename: str):
+    self.model = None
+    try:
+      self.model = load_model(model_filename)
+    except:
+      raise Exception("Model didn't load successfully")
+
+  def load_model(self,model_filename):
+    try:
+      self.model = load_model(model_filename)
+    except:
+      raise Exception("Model didn't load successfully")
+
+
+  def eval(self, features):
+    if(self.model):
+      sample=features.reshape(1,features.shape[0],features.shape[1])
+      pred=self.model.predict(sample)
+      return pred[0]
+    else:
+      raise  Exception("Model isn't loaded yet")
+
